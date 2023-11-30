e392c02e8f017f5b603fbd1f7442d6d660e03c1e
Sam Holmberg <sam@holmberg.dev>
2023-11-30 09:27:28 -0800

    update cal link

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   name: "Sam Holmberg",
-  cal: "https://calendar.google.com/calendar/ical/d721a6f85d1f6a5df9ef3efa7d550de29c3581e4cede18de74329a681a33bb8b%40group.calendar.google.com/public/basic.ics",
+  cal: "https://calendar.google.com/calendar/ical/171cf08cb0b78bf7208dbdd66d21918da3b1c029176e7343d84dc8070e8729bf%40group.calendar.google.com/public/basic.ics",
   plans: [
     "https://calendar.google.com/calendar/ical/sammyboy1510%40gmail.com/public/basic.ics",
     "https://calendar.google.com/calendar/ical/dae10e6cf457de27dbf5ba4da2bccb74a418540f75823a5004f044ad9ef454d0%40group.calendar.google.com/public/basic.ics",
